d658418a8825f851b8cbafd57047ca9acaff0aa0
Mykhailo Orydoroha <orydoroha.mykhailo@gmail.com>
2021-04-06 17:38:30 +0300

    changed main for safer usage of enum

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <thread>
-#include "log.h"
+#include "log.hpp"
 
 /*
 Example of console output:
@@ -16,11 +16,12 @@ Example of log2.txt:
 23.02.2020 18:06:06; ERROR; f3(3444): My int is 123;
 */
 
+using llv = logLvl;
 
 void f1() {
     auto consoleLogger = getLogger(); // Log to console
     // Console output: 23.02.2020 18:06:06; DEBUG; (2561): Running a thread
-    consoleLogger(DEBUG) << "Running a thread";
+    consoleLogger(llv::DEBUG) << "Running a thread";
 
     auto fileLogger = getLogger("log.txt"); // Log to file with name log.txt
     // File output: 23.02.2020 18:06:06; INFO; (2561): Running a thread
@@ -30,11 +31,11 @@ void f1() {
 void f2() {
     auto logger = getLogger("log.txt", "f2"); // Log to file with name log.txt. Each message has f2 prefix
     // File output: 23.02.2020 18:06:06; DEBUG; f2(3444): Running a thread
-    logger(DEBUG) << "Running a thread";
+    logger(llv::DEBUG) << "Running a thread";
 
     double timeSpent = 10.0;
     // File output: 23.02.2020 18:06:06; WARNING; f2(3444): Time spent in the thread: 10.0 seconds
-    logger(WARNING) << "Time spent in the thread: " << timeSpent << " seconds";
+    logger(llv::WARNING) << "Time spent in the thread: " << timeSpent << " seconds";
 }
 
 void f3() {
@@ -44,7 +45,7 @@ void f3() {
 
     int i = 123;
     // File output: 23.02.2020 18:06:06; ERROR; f3(3444): My int is 123;
-    logger(ERROR) << "My int is " << i;
+    logger(llv::ERROR) << "My int is " << i;
 }
 
 int main() {
