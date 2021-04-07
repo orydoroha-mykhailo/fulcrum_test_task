a5c7e2b69857f90204a068edd86cbd020b6d72af
Mykhailo Orydoroha <orydoroha.mykhailo@gmail.com>
2021-04-07 11:56:40 +0300

    close #4

--- log.hpp
+++ log.hpp
@@ -17,32 +17,39 @@ class Logger{
 		Logger() : isFile(false) {}
 		Logger(const std::string& fileName, const std::string& prefix = ""): prefix(prefix)
 		{
-			fout.open(fileName);
+			fout.open(fileName, std::ios::app);
 		}
 		Logger& operator()(const logLvl& lvl);
 
 		template<typename T>
 		Logger& operator<<(const T& message)
 		{
-			time_t now = time(0);
-			std::stringstream ss;
-			std::string time = ctime(&now);
+			std::time_t now = std::time(0);
+			time = std::ctime(&now);
 			time.pop_back();
-			ss << time
+			logs << message;
+			return *this;
+		}
+		~Logger()
+		{
+			if(isFile)
+			if(fout.good())
+		{
+			fout << time
 				<< "; "<< whatLevel() 
 				<< "; "<< prefix << "(" << std::this_thread::get_id() 
-				<< "): " << message << std::endl;
-			if(isFile)
-				if(fout.good())
-					fout << ss.str();
-					else{}
-			else 
-				std::cout << ss.str();
-			return *this;
+				<< "): " << logs.str() << std::endl;
+		}else{}
+		else
+			std::cout << time
+				<< "; "<< whatLevel() 
+				<< "; "<< prefix << "(" << std::this_thread::get_id() 
+				<< "): " << logs.str() << std::endl;
 		}
   private:
 		std::string whatLevel() const;
-
+		std::string time;
+		std::ostringstream logs;
 		const bool isFile = true;
 		std::ofstream fout;
 		const std::string prefix = "";
--- main.cpp
+++ main.cpp
@@ -19,39 +19,39 @@ Example of log2.txt:
 using llv = logLvl;
 
 void f1() {
-    auto consoleLogger = getLogger(); // Log to console
+    //auto consoleLogger = getLogger(); // Log to console
     // Console output: 23.02.2020 18:06:06; DEBUG; (2561): Running a thread
-    consoleLogger(llv::DEBUG) << "Running a thread";
+    getLogger()(llv::DEBUG) << "Running a thread";
 
-    auto fileLogger = getLogger("log.txt"); // Log to file with name log.txt
+    //auto fileLogger = getLogger("log.txt"); // Log to file with name log.txt
     // File output: 23.02.2020 18:06:06; INFO; (2561): Running a thread
-    fileLogger << "Running a thread"; // Default log level INFO
+    getLogger("log.txt") << "Running a thread"; // Default log level INFO
 }
 
 void f2() {
-    auto logger = getLogger("log.txt", "f2"); // Log to file with name log.txt. Each message has f2 prefix
+    //getLogger("log.txt", "f2"); // Log to file with name log.txt. Each message has f2 prefix
     // File output: 23.02.2020 18:06:06; DEBUG; f2(3444): Running a thread
-    logger(llv::DEBUG) << "Running a thread";
+    getLogger("log.txt", "f2")(llv::DEBUG) << "Running a thread";
 
     double timeSpent = 10.0;
     // File output: 23.02.2020 18:06:06; WARNING; f2(3444): Time spent in the thread: 10.0 seconds
-    logger(llv::WARNING) << "Time spent in the thread: " << timeSpent << " seconds";
+    getLogger("log.txt", "f2")(llv::WARNING) << "Time spent in the thread: " << timeSpent << " seconds";
 }
 
 void f3() {
-    auto logger = getLogger("log2.txt", "f3");  // Log to file with name log2.txt. Each message has f3 prefix
+    //auto logger = getLogger("log2.txt", "f3");  // Log to file with name log2.txt. Each message has f3 prefix
     // File output: 23.02.2020 18:06:06; INFO; f3(3444): Running a thread
-    logger << "Running a thread"; // Default log level INFO
+    getLogger("log2.txt", "f3") << "Running a thread"; // Default log level INFO
 
     int i = 123;
     // File output: 23.02.2020 18:06:06; ERROR; f3(3444): My int is 123;
-    logger(llv::ERROR) << "My int is " << i;
+    getLogger("log2.txt", "f3")(llv::ERROR) << "My int is " << i;
 }
 
 int main() {
-    auto logger = getLogger(); // Log to console
+    //auto logger = getLogger(); // Log to console
     // Console output: 23.02.2020 18:06:06; INFO; (1020): Starting the app
-    logger << "Starting the app"; // Default log level INFO
+    getLogger() << "Starting the app"; // Default log level INFO
 
     std::thread t1(f1);
     std::thread t2(f2);
